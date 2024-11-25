07e62bf468ea513d1376eb2d50eb312597e6c35b
Saugata WinLaptop <kundusaugata576@gmail.com>
2024-11-25 22:12:06 +0530

    working struct

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,7 +10,7 @@ const Card: React.FC<CardProps> = ({ title, description, imageUrl }) => {
   return (
     <div>
     <div className=" relative max-w-sm rounded mt-8  bg-[#D9D9D9]">
-        <img className=" absolute left-36 bottom-96 object-cover" src={imageUrl} alt={title} />
+        <img className=" relative left-36 object-cover" src={imageUrl} alt={title} />
         <div className="p-4 text-center  ">
         <h2 className="font-bold text-4xl mb-5 mt-5">{title}</h2>
         <p className="text-black text-xl">{description}</p>
