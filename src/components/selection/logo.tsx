774e3cabbb301920ed19e92c8adf09ff74f20a23
Stuti Sinha <stutisinha712@gmail.com>
2024-11-25 14:14:02 +0530

    selection page

---
+++ src/components/selection/logo.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react'
+import Image from 'next/image'
+
+const Logo = () => {
+return (
+    <div>
+    
+      <div>
+        <Image
+        className=""
+        src="/Main logo.svg"
+        alt="Next.js logo"
+        width={160}
+        height={160}
+        />
+      </div>
+    </div>
+
+    )
+}
+export default Logo
