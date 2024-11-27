c52c022ff58bb437f51059f2c7789f8cfbd33d0e
samprit <sampritdas2004@gmail.com>
2024-11-27 13:30:33 +0530

    react import

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,6 +1,7 @@
 import Footer from "@/components/register/Footer";
 import SignupForm from "@/components/register/Form";
 import Header from "@/components/register/header";
+import React from "react";
 
 export default function Register() {
   return (
