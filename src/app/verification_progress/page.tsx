8ade2ee746526e6b43fe84cb19f1a5a0c501278b
Stuti Sinha <stutisinha712@gmail.com>
2024-11-27 14:02:33 +0530

    verification final stage page

--- src/app/verification_progress/page.tsx
+++ src/app/verification_progress/page.tsx
@@ -1,10 +1,12 @@
 import Logo from "@/components/logo";
 import Footer from "@/components/verification/footer";
+import Header from "@/components/verification/header";
 export default function VerificationProgress() {
     return (
         
         <>
         <Logo />
+        <Header />
         <Footer />
         </>
        
