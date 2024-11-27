7a069eb0ef5d996b4179d78cae5e3e1f50fbaebc
samprit <sampritdas2004@gmail.com>
2024-11-28 00:50:42 +0530

    button fixed

--- src/components/overview/Side_nav.tsx
+++ src/components/overview/Side_nav.tsx
@@ -4,9 +4,9 @@ const Side_nav = () => {
   return (
     <div className='mt-6 bg-black h-[100vh] w-[15%] flex justify-center rounded-r-3xl sticky'>
       <div className="flex flex-col gap-5 mt-14  ">
-        <div className='text-md font-semibold bg-white px-5 py-3 rounded-lg  '>Project Overview</div>
-        <div className='text-md font-semibold text-white px-5 py-3'>Draft Proposal</div>
-        <div className='text-md font-semibold text-white px-5 py-3'>Submit Proposal</div>
+        <button className='text-md font-semibold bg-white px-5 py-3 rounded-lg  '>Project Overview</button>
+        <button className='text-md font-semibold text-white px-5 py-3'>Draft Proposal</button>
+        <button className='text-md font-semibold text-white px-5 py-3'>Submit Proposal</button>
       </div>
     </div>
   )
