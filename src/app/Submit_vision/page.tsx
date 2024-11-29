c240901c1ad89381ca192fb092c253356ca01e2a
samprit <sampritdas2004@gmail.com>
2024-11-29 19:02:45 +0530

    submit_vision initial stage

---
+++ src/app/Submit_vision/page.tsx
@@ -0,0 +1,17 @@
+import Form from "@/components/Submit_vision/Form";
+import Nav from "@/components/Submit_vision/Nav";
+
+
+
+
+
+export default function Submit_vision(){
+    return (
+    <>
+        <Nav/>
+        <Form/>
+        
+    </>
+
+    );
+ }
