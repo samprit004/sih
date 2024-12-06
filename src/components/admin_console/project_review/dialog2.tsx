38e7bb1f065b27e67ed50dec5439d9d0fe73efc7
Stuti Sinha <stutisinha712@gmail.com>
2024-12-06 13:16:38 +0530

    dialogbox

--- src/components/admin_console/project_review/dialog2.tsx
+++ src/components/admin_console/project_review/dialog2.tsx
@@ -1,6 +1,14 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-import { format, addMonths, subMonths, startOfMonth, endOfMonth, eachDayOfInterval, getDay } from 'date-fns';
+import {
+    format,
+    addMonths,
+    subMonths,
+    startOfMonth,
+    endOfMonth,
+    eachDayOfInterval,
+    getDay,
+} from 'date-fns';
 
 interface Dialog2Props {
     isOpen: boolean;
@@ -18,7 +26,13 @@ const Dialog2: React.FC<Dialog2Props> = ({ isOpen, onClose }) => {
             const slots = Math.floor(Math.random() * 4); // Random 0-3 slots
             return {
                 slots,
-                times: slots > 0 ? ['10:00 AM - 12:00 PM', '1:00 PM - 3:00 PM', '4:00 PM - 6:00 PM'].slice(0, slots) : []
+                times:
+                    slots > 0
+                        ? ['10:00 AM - 12:00 PM', '1:00 PM - 3:00 PM', '4:00 PM - 6:00 PM'].slice(
+                              0,
+                              slots
+                          )
+                        : [],
             };
         });
     };
@@ -38,10 +52,10 @@ const Dialog2: React.FC<Dialog2Props> = ({ isOpen, onClose }) => {
 
         // Fill the first week with empty days if the month doesn't start on Sunday
         const emptyDays = Array(firstDay).fill(null); // Empty spaces before the 1st date of the month
-        
+
         return {
             days: [...emptyDays, ...days], // Combine empty days with the actual days of the month
-            daysOfWeek: formattedDaysOfWeek
+            daysOfWeek: formattedDaysOfWeek,
         };
     };
 
@@ -53,7 +67,7 @@ const Dialog2: React.FC<Dialog2Props> = ({ isOpen, onClose }) => {
     };
 
     const handleDateClick = (date: Date) => {
-        const day = days.find(day => day && day.getDate() === date.getDate());
+        const day = days.find((day) => day && day.getDate() === date.getDate());
         if (day) {
             setSelectedDate(format(date, 'yyyy-MM-dd'));
             setAvailableSlots(daySlots[date.getDate() - 1]?.times || []);
@@ -78,24 +92,36 @@ const Dialog2: React.FC<Dialog2Props> = ({ isOpen, onClose }) => {
         isOpen && (
             <div className="fixed inset-0 bg-black bg-opacity-30 flex justify-center items-center">
                 <div className="">
-                    <div className="bg-white p-2 border-2 border-black h-full rounded-lg">
+                    <div className="bg-white p-2 border-2 border-black h-full w-[900px] rounded-lg">
                         {/* Close Button */}
-                        <div className='flex justify-end'>
-                        <button
-                            onClick={onClose}
-                            className="text-black h-10 w-10 border-black border-2 rounded-full p-2 hover:bg-gray-300"
-                        >
-                          <b>  ✕</b>
-                        </button>
+                        <div className="flex justify-end">
+                            <button
+                                onClick={onClose}
+                                className="text-black h-10 w-10 border-black border-2 rounded-full p-2 hover:bg-gray-300"
+                            >
+                                <b> ✕</b>
+                            </button>
                         </div>
                         {/* Calendar */}
-                        <div className="flex justify-center ml-16 p-2 items-center gap-10">
+                        <div className="flex  ml-6 p-2 items-center gap-10">
                             <div>
-                                <div className='border-2 mb-2 border-black p-2 rounded-md'>
-                                    <div className="flex items-center  justify-between mb-4">
-                                        <button onClick={() => handleMonthChange('prev')} className="p-2 bg-gray-200 rounded">&lt;</button>
-                                        <h3 className="text-lg underline">{format(currentMonth, 'MMMM yyyy')}</h3>
-                                        <button onClick={() => handleMonthChange('next')} className="p-2 bg-gray-200 rounded">&gt;</button>
+                                <div className="border-2 mb-2 border-black p-2 rounded-md">
+                                    <div className="flex items-center justify-between mb-4">
+                                        <button
+                                            onClick={() => handleMonthChange('prev')}
+                                            className="p-2 bg-gray-200 rounded"
+                                        >
+                                            &lt;
+                                        </button>
+                                        <h3 className="text-lg underline">
+                                            {format(currentMonth, 'MMMM yyyy')}
+                                        </h3>
+                                        <button
+                                            onClick={() => handleMonthChange('next')}
+                                            className="p-2 bg-gray-200 rounded"
+                                        >
+                                            &gt;
+                                        </button>
                                     </div>
 
                                     {/* Days of the Week */}
@@ -127,30 +153,34 @@ const Dialog2: React.FC<Dialog2Props> = ({ isOpen, onClose }) => {
                                 {/* Status Indicators */}
                                 <div className="flex gap-2">
                                     <div className="flex items-center justify-center gap-2 w-36 h-7 border rounded-md border-black">
-                                        <div className="h-5 w-5 border-2 rounded-md border-red-700 "></div> Fully Occupied
+                                        <div className="h-5 w-5 border-2 rounded-md border-red-700 "></div>{' '}
+                                        Fully Occupied
                                     </div>
                                     <div className="flex items-center justify-center gap-2 w-36 h-7 border rounded-md border-black">
-                                        <div className="h-5 w-5 border-2 rounded-md border-green-700 "></div> Fully Available
+                                        <div className="h-5 w-5 border-2 rounded-md border-green-700 "></div>{' '}
+                                        Fully Available
                                     </div>
                                 </div>
                                 <div className="flex items-center justify-center gap-2 w-40 h-7 border rounded-md border-black mt-2 ml-14">
-                                    <div className="h-5 w-5 border-2 rounded-md border-yellow-700 "></div> Partially Occupied
+                                    <div className="h-5 w-5 border-2 rounded-md border-yellow-700 "></div>{' '}
+                                    Partially Occupied
                                 </div>
                             </div>
 
                             {/* Time Slot Review */}
-                            <div className="">
-                                <h4 className="text-xl font-semibold underline mb-4">Available Time Slots</h4>
-                                <div className="flex gap-2">
-                                    {availableSlots.map((slot, index) => (
-                                        <button
-                                            key={index}
-                                            className="p-2 bg-gray-200 rounded mb-10"
-                                        >
-                                            {slot}
-                                        </button>
-                                    ))}
-                                </div>
+                            <div>
+                                <h4 className="text-xl font-semibold underline mb-3">Available Time Slots</h4>
+                                {availableSlots.length > 0 ? (
+                                    <div className="flex gap-2">
+                                        {availableSlots.map((slot, index) => (
+                                            <button key={index} className="p-2 bg-gray-200 rounded mb-4">
+                                                {slot}
+                                            </button>
+                                        ))}
+                                    </div>
+                                ) : (
+                                    <div className="text-red-500 font-semibold mb-4">No slots available</div>
+                                )}
 
                                 <div className="font-semibold underline text-xl mb-2">Time Slot Review</div>
 
@@ -158,28 +188,36 @@ const Dialog2: React.FC<Dialog2Props> = ({ isOpen, onClose }) => {
                                 <table className="w-96 mb-4 border-2 border-black rounded-lg">
                                     <thead>
                                         <tr>
-                                            <th className="border border-gray-300 p-2 bg-black text-white">Date</th>
-                                            <td className="border border-gray-300 p-2">{selectedDate || 'N/A'}</td>
+                                            <th className="border border-gray-300 p-2 bg-black text-white">
+                                                Date
+                                            </th>
+                                            <td className="border border-gray-300 p-2">
+                                                {selectedDate || 'N/A'}
+                                            </td>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <th className="border border-gray-300 p-2 bg-black text-white">Time</th>
+                                            <th className="border border-gray-300 p-2 bg-black text-white">
+                                                Time
+                                            </th>
                                             <td className="border border-gray-300 p-2">
-                                                {availableSlots.length > 0 ? availableSlots.join(' or  ') : 'N/A'}
+                                                {availableSlots.length > 0
+                                                    ? availableSlots.join(' or ')
+                                                    : 'N/A'}
                                             </td>
                                         </tr>
                                     </tbody>
                                 </table>
 
-                              <div className='flex justify-center'>
-                                <button
-                                    onClick={handleFix}
-                                    className="p-2 bg-black text-white rounded w-28"
-                                    disabled={!selectedDate || availableSlots.length === 0}
-                                >
-                                    Fix
-                                </button>
+                                <div className="flex justify-center">
+                                    <button
+                                        onClick={handleFix}
+                                        className="p-2 bg-black text-white rounded w-28"
+                                        disabled={!selectedDate || availableSlots.length === 0}
+                                    >
+                                        Fix
+                                    </button>
                                 </div>
                             </div>
                         </div>
