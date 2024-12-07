f62b5b4ecf0be5f8fb3ee4efb1f536ed6e76e0ea
Stuti Sinha <stutisinha712@gmail.com>
2024-12-07 20:47:13 +0530

    design fixes

--- src/app/login/PI/page.tsx
+++ src/app/login/PI/page.tsx
@@ -2,12 +2,14 @@ import Logo from "@/components/logo";
 import Caption from "@/components/pi_login/caption";
 import Form from "@/components/pi_login/form";
 import Rectangle2 from "@/components/blackbox";
+
 export default function Login(){
     return (
       <>
         <Logo />
         <Caption/>
         <Form/>
+        
         <Rectangle2/>
         
       </>
