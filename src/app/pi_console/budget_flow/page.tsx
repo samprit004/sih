ba5289333c407f737bd3a31e88c6e7ac2c3fb959
Stuti Sinha <stutisinha712@gmail.com>
2024-12-12 03:55:04 +0530

    side nav

---
+++ src/app/pi_console/budget_flow/page.tsx
@@ -0,0 +1,8 @@
+import Side_nav from "@/components/pi_console/budget_flow/side_nav";
+export default function Proposal_tracker() {
+    return (
+      <>
+        <Side_nav />
+      </>
+    );
+  }
---
+++ src/app/pi_console/proposal_tracker/page.tsx
@@ -0,0 +1,8 @@
+import Side_nav from "@/components/pi_console/proposal_tracker/side_nav";
+export default function Proposal_tracker() {
+    return (
+      <>
+        <Side_nav />
+      </>
+    );
+  }
